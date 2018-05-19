16b0c04aa11fc9e167311bbfaa667c0be4583875
cfreshman <cyruswfreshman@gmail.com>
2018-05-19 15:46:19 +1200

    Added POST/GET for jobs

--- server/jobs/model.js
+++ server/jobs/model.js
@@ -1,10 +1,29 @@
-const jobs = [];
+const jobs = [{
+  jobId: '1',
+  prompt: 'Discuss the arrival of the first polynesians in New Zealand',
+  ownerId: 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
+  createdAt: '2018-05-19',
+  duration: 86400,
+  minSources: 8,
+  minRating: 2,
+  reward: 8,
+  contractDuration: 86400
+}];
 
+let count = 1000;
 
-function createJob (prompt) {
+function createJob (params) {
   return new Promise((resolve, reject) => {
     let job = {
-      prompt
+      jobId: String(count++),
+      prompt: params.prompt,
+      ownerId: params.userId,
+      createdAt: Date.now(),
+      duration: params.duration || 259200, // 3 days
+      minSources: params.minSources || 3,
+      minRating: params.minRating || 3,
+      reward: params.reward,
+      contractDuration: params.contractDuration || 10800 // 3 hours
     };
     jobs.push(job);
     resolve(job);
@@ -15,6 +34,11 @@ function getJobs () {
   return Promise.resolve(jobs);
 }
 
+function getJob (jobId) {
+  return Promise.resolve(jobs.find(job => job.jobId === jobId));
+}
+
 
 exports.createJob = createJob;
 exports.getJobs = getJobs;
+exports.getJob = getJob;
--- server/jobs/routes.js
+++ server/jobs/routes.js
@@ -4,59 +4,33 @@ const routes = express.Router();
 const model = require('./model');
 
 routes.get('/', (req, res) => {
-  /*
-  let payload = {
-    jobs: [
-      {
-        prompt: 'Discuss the arrival of the first polynesians in New Zealand',
-        owner: 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
-        createdAt: '2018-05-19',
-        duration: 86400,
-        numSources: 8,
-        minRating: 2,
-        reward: 8,
-        jobId: 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
-        contractDuration: 86400
-      }
-    ]
-  };
-  */
   model.getJobs().then(jobs => {
     let payload = {
       jobs
     };
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(payload));
-  })
+  });
 });
 
 routes.get('/:id', (req, res) => {
-  let payload = {
-    job: {
-      prompt: 'Discuss the arrival of the first polynesians in New Zealand',
-      owner: 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
-      createdAt: '2018-05-19',
-      duration: 86400,
-      numSources: 8,
-      minRating: 2,
-      reward: 8,
-      jobId: 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
-      contractDuration: 86400
-    }
-  };
-
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(payload));
+  model.getJob(req.params.id).then(job => {
+    let payload = {
+      job
+    };
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(payload));
+  });
 });
 
 routes.post('/', (req, res) => {
-  model.createJob(req.body.prompt).then(() => {
+  model.createJob(req.body).then(() => {
     let payload = {
       error: false
     };
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(payload));
-  });  
+  });
 });
 
 module.exports = routes;

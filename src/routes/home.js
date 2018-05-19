abea5ef1192025a7402384eefd0c6c0e7f6ea522
Christian Scott <christianfscott@gmail.com>
2018-05-19 16:45:48 +1200

    Add "no jobs" message

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,9 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import Profile from "../components/Profile";
 import AddJobButton from "../components/AddJobButton";
+
+const P = styled.p`
+  margin-top: 1rem;
+  font-size: 1.6rem;
+  text-align: center;
+  color: ${props => props.theme.secondary.dark};
+`
 
 export default () =>
   <React.Fragment>
     <Profile />
+
+    <P>You don't have any jobs</P>
+
     <AddJobButton />
   </React.Fragment>

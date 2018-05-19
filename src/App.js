53d924a1873938bb8943a21a38249bc317a21e5c
Christian Scott <christianfscott@gmail.com>
2018-05-19 13:47:04 +1200

    Fix warnings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route } from "react-router";
 import routes from "./routes";
 import Page from "./components/Page";

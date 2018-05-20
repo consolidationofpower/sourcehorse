e48cfc75f59d2d6d8b542a2003af1a6a9775b85b
justinvos <justinvosnz@gmail.com>
2018-05-20 15:48:20 +1200

    Fix listing header

--- src/routes/listings.js
+++ src/routes/listings.js
@@ -10,7 +10,6 @@ export default class Listing extends React.Component {
     };
   }
 
-
   componentDidMount() {
     api.getJobs().then(result => {
       console.log('result', result);
@@ -23,8 +22,9 @@ export default class Listing extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Listings</h1>
-        <p>dkjncjkebcjhb</p>
+        <ListingHeader>
+          <h1>Listings</h1>
+        </ListingHeader>
         <AnswerListing>
           {this.state.jobs.map(job => <AnswerJob key={job.id} {...job} />)}
         </AnswerListing>
@@ -33,15 +33,30 @@ export default class Listing extends React.Component {
   }
 }
 
+function toCurrency(num) {
+  let roundedNum = Math.round(num * 10) / 10;
+  return `${roundedNum}`
+}
+
 const AnswerJob = (props) => (
-  <React.Fragment>
-    <AnswerJobCard>
-      <AnswerJobTitle>{props.title || 'Untitled'}</AnswerJobTitle>
-    </AnswerJobCard>
-  </React.Fragment>
+  <AnswerJobCard>
+    <AnswerJobMain>
+      <AnswerJobLeft>
+        <AnswerJobTitle>{props.title || 'Untitled'}</AnswerJobTitle>
+        <AnswerJobSources>{props.min_sources || 1} Sources</AnswerJobSources>
+      </AnswerJobLeft>
+      <AnswerJobReward>${toCurrency(props.reward) || '0.0'}0</AnswerJobReward>
+    </AnswerJobMain>
+    <AnswerJobExtra>
+      hi there extra extra
+    </AnswerJobExtra>
+  </AnswerJobCard>
 );
 
-const AnswerJobTitle = styled.p`
+const ListingHeader = styled.div`
+  background: ${props => props.theme.gradient};
+  color: white;
+  padding: 15px;
 `;
 
 const AnswerListing = styled.div`
@@ -53,4 +68,29 @@ const AnswerJobCard = styled.div`
   background: ${props => props.theme.secondary.main};
   margin-top: 10px;
   padding: 25px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
+
+const AnswerJobLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const AnswerJobTitle = styled.p`
+  font-size: 22px;
+`;
+
+const AnswerJobSources = styled.p`
+`;
+
+const AnswerJobReward = styled.p`
+  font-size: 32px;
+`;
+
+const AnswerJobMain = styled.div`
+`
+
+const AnswerJobExtra = () => `
+`
